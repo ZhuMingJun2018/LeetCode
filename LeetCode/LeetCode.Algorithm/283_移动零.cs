4052b98f9b995674cdac657bea90f84dfff9a297
mingjun.zhu <ZhuMingJun2018@Github.com>
2021-11-10 19:59:40 +0800

    no message

---
+++ LeetCode/LeetCode.Algorithm/283_移动零.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Algorithm
+{
+    public class _283_移动零
+    {
+        public static void MoveZeroes(params int[] nums)
+        {
+            int length = nums.Length;
+
+            var list = nums.ToList();
+
+            int count = list.RemoveAll(num => num == 0);
+
+            list.AddRange(new int[count]);
+
+            for (int i = 0; i < length; i++)
+            {
+                nums[i] = list[i];
+            }
+        }
+    }
+}
