6d1cd31ea31f95500cbe2331de36b9635d4cc55c
mingjun.zhu <ZhuMingJun2018@Github.com>
2021-11-26 20:40:57 +0800

    no message

---
+++ LeetCode/LeetCode/数据库.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    internal class 数据库 : DbContext
+    {
+        #region 183. 从不订购的客户
+
+        public void 从不订购的客户()
+        {
+            from customer in Customers
+            where Orders.FirstOrDefault(order => order.CustomerId == customer.Id) == null
+            select customer;
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+
+        public class Customer
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+        }
+        public class Order
+        {
+            public int Id { get; set; }
+            public int CustomerId { get; set; }
+        } 
+        #endregion
+    }
+}
--- LeetCode/LeetCode/设计.cs
+++ LeetCode/LeetCode/设计.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,11 +9,37 @@ namespace LeetCode
 {
     internal class 设计
     {
+        #region 933. 最近的请求次数 2021-11-26 20:23:05
+        public class RecentCounter
+        {
+            int[] requests = new int[100000];
+            int id = 0;
+
+            public RecentCounter()
+            {
+
+            }
+
+            public int Ping(int t)
+            {
+                requests[id++] = t;
+                int i = id - 1;
+                int count = 0;
+                while (i > -1 && requests[i] + 300 >= t)
+                {
+                    i--;
+                    count++;
+                }
+                return count;
+            }
+        }
+        #endregion
+
         #region 1603. 设计停车系统 2021-11-25 20:51:53
         //https://leetcode-cn.com/problems/design-parking-system/
         public class ParkingSystem
         {
-            Dictionary<int,int> Dic { get; set; }
+            Dictionary<int, int> Dic { get; set; }
 
 
             public ParkingSystem(int big, int medium, int small)
